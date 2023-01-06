25671a1e6083e437cad2d76adceb86905155892b
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-01-06 22:26:23 +0000

    Work on challenge 1

--- script.js
+++ script.js
@@ -18,13 +18,28 @@
 
 // Constant Time O(1) - excellent
 
-const boxes = [0, 1, 2, 3, 4, 5];
+// const boxes = [0, 1, 2, 3, 4, 5];
 
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]);  //O(1)
-  console.log(boxes[1]);  //0(2)
-}
+// function logFirstTwoBoxes(boxes) {
+//   console.log(boxes[0]);  //O(1)
+//   console.log(boxes[1]);  //0(2)
+// }
 
-logFirstTwoBoxes(boxes)
+// logFirstTwoBoxes(boxes)
 
+//Exercise 1
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+  // function funChallenge(input) {
+  //   let a = 10;
+  //   a = 50 + 3;
+
+  //   for (let i = 0; i < input.length; i++) {
+  //     anotherFunction();
+  //     let stranger = true;
+  //     a++;
+  //   }
+  //   return a;
+  // }
+
+  // Answer: O(n) linear time
 
