4f7e99911e3d4833058651a265f5374230eb4ebe
Elizabeth Hollinger <>
2023-01-06 21:39:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+findNemo(nemo);
+
