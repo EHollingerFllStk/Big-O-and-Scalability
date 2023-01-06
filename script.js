a2d5589525cc1a293488bf42cb529f1fbd3645a5
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-01-06 22:21:47 +0000

    Identify Linear time and constant time comparison

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(100).fill('nemo');
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO!');
-    }
-  }
-  let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
+//Linear Time - O(n) - good
+
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const large = new Array(100).fill('nemo');
+
+// function findNemo(array) {
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO!');
+//     }
+//   }
+
+// }
+
+// findNemo(large);  // 0(n) --> Linear Time
+
+// Constant Time O(1) - excellent
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]);  //O(1)
+  console.log(boxes[1]);  //0(2)
 }
 
-findNemo(large);
+logFirstTwoBoxes(boxes)
+
 
